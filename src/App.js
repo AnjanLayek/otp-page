7b58b1836a3d1cacdcace3a96efa1c9fc4570223
Anjan <anjanlayek001@gmail.com>
2023-01-16 17:49:15 +0530

    committing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="input">
+          <input type="text" maxLength={1} />
+          <input type="text" maxLength={1} />
+          <input type="text" maxLength={1} />
+          <input type="text" maxLength={1} />
+          <input type="text" maxLength={1} />
+          <input type='text' maxLength={1}/>
+        </div>
+        <div className="submit">
+          <button className="btn">Verify</button>
+        </div>
+      </div>
     </div>
   );
 }
